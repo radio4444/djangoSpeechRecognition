5b17cb1102c9c21fe68f78ac893f08199966b86a
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-13 10:23:04 -0500

    Added html file called helloWorld.html and change return for the request in views.py

--- practice/views.py
+++ practice/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 
 def practice(request):
-	return HttpResponse('<h1>Hello world</h1>')
+	return render(request, 'practice/helloWorld.html')
