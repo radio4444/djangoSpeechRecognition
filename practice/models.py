0b886e57fb21ca5c3161ece9b54e7ed5694aca36
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-14 10:19:14 -0500

    Added UserInfo(table) fields

--- practice/models.py
+++ practice/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class UserInfo(models.Model):
+	name = models.TextField()
+	email = models.EmailField()
+	address = models.TextField()
+	phoneNumber = models.IntegerField()
