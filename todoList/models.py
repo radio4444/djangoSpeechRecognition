abf16f98ae75e7960f8ea410eca6c866ed64dff9
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-16 10:59:17 -0500

    Updated the models.py

--- todoList/models.py
+++ todoList/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 # Create your models here.
 class TodoList(models.Model):
-	todoListName = models.CharField(max_length=100)
+	PRIORITY_CHOICES = [
+		('high', 'High'),
+		('med', 'Medium'),
+		('low', 'Low'),
+	]
+	task_name = models.CharField(max_length=200)
 	dueDate = models.DateTimeField()
-	checkBox = models.BooleanField()
-	priority = models.TextChoices('High', 'Med', 'Low')
+	completed = models.BooleanField(default=False)
+	priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+
+	def __str__(self):
+		return self.task_name
