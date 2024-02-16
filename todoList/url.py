adebfd110abb4d376f73fded65dd12d590811591
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-16 11:12:21 -0500

    Updated function in views. Added add_todo_item.html file. Updated url.py accordingly

--- todoList/url.py
+++ todoList/url.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.testing, name='testing')
+	path('', views.testing, name='testing'),
+	path('add/', views.add_todo_item, name='add_todo_item'),
+	path('complete/<int:todo_id>/', views.complete_todo_items, name='complete_todo_item'),
 ]
--- todoList/views.py
+++ todoList/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import TodoList
 from .form import TodoListForm
 
@@ -9,13 +9,24 @@ def testing(request):
 	return render(request, 'todoList/testing.html')
 
 
-def todolist_form(request):
+def todolist(request):
+	todo_items = TodoList.objects.filter(completed=False)
+	return render(request, 'todoList/createTodo.html', {'todo_items': todo_items})
+
+
+def complete_todo_items(request, todo_id):
+	todo_item = TodoList.objects.filter(id=todo_id)
+	todo_item.completed = False
+	todo_item.save()
+	return redirect('todo_list')
+
+
+def add_todo_item(request):
 	if request.method == 'POST':
 		form = TodoListForm(request.POST)  # Create form using the data
 		if form.is_valid():
 			form.save()
+			return redirect('todo_list')
 		else:
 			form = TodoListForm()  # Empty form
-		todolist = TodoList.objects.all()
-
-		return render(request, 'todoList/createTodo.html',)
+		return render(request, 'todoList/add_todo_item.html', {'form': form})
