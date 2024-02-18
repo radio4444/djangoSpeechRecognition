54577e2014afc70f1f287f3d78dc08d33fd2f3d8
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-18 12:43:32 -0500

    Added delete_todo_item in urlpatterns

--- djangoSpeechRecognition/urls.py
+++ djangoSpeechRecognition/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from todolist.views import todo_list, complete_todo_item, add_todo_item
+from todolist.views import todo_list, complete_todo_item, add_todo_item, delete_todo_item
 
 urlpatterns = [
     path('todo/', todo_list, name='todo_list'),
     path('todo/complete/<int:todo_id>/', complete_todo_item, name='complete_todo_item'),
+    path('todo/delete/<int:todo_id>/', delete_todo_item, name='delete_todo_item'),
     path('todo/add/', add_todo_item, name='add_todo_item'),
 ]
 
