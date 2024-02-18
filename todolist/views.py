c315467301d8b6820031ffb64bf8b995ed63446b
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-18 12:42:13 -0500

    Added delete_todo_item function in views.py

--- todolist/views.py
+++ todolist/views.py
@@ -5,17 +5,24 @@ from .forms import TodoItemForm
 
 def todo_list(request):
 	incomplete_items = TodoItem.objects.filter(completed=False)
+	# Completed_items are shown in the todoList_html
 	completed_items = TodoItem.objects.filter(completed=True)
 	return render(request, 'todolist/todo_list.html',
 	              {'incomplete_items': incomplete_items, 'completed_items': completed_items})
 
 
+# User mark the task complete.
 def complete_todo_item(request, todo_id):
 	todo_item = TodoItem.objects.get(id=todo_id)
 	todo_item.completed = True
 	todo_item.save()
 	return redirect('todo_list')
 
+
+def delete_todo_item(request, todo_id):
+	todo_item = TodoItem.objects.filter(id=todo_id).delete()
+	todo_item.save()
+	return redirect('todo_list')
 
 def add_todo_item(request):
 	if request.method == 'POST':
