285036fc1944de7802761d4848b5542ef0456dd9
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-18 12:52:33 -0500

    Modified the delete_todo_item in views.py. Added deleted function in html.

--- todolist/views.py
+++ todolist/views.py
@@ -21,7 +21,6 @@ def complete_todo_item(request, todo_id):
 
 def delete_todo_item(request, todo_id):
 	todo_item = TodoItem.objects.filter(id=todo_id).delete()
-	todo_item.save()
 	return redirect('todo_list')
 
 def add_todo_item(request):
