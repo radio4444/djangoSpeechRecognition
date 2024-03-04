acc803fe2a726d78bf7af0664b763efb8482baff
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-03-04 09:25:45 -0500

    - Add register function in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def register(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('login')
+	else:
+		form = RegistrationForm()
+	return render(request, 'accounts/register.html', {'form':form})
+
+
