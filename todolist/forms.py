779e7608b6328be79871257ed84a545a7a3a969b
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-03-04 09:03:25 -0500

    Reformat form in todoList. Create account startapp

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import TodoItem
 
+
 class TodoItemForm(forms.ModelForm):
-    class Meta:
-        model = TodoItem
-        fields = ['task_name', 'priority', 'due_date', 'description']
+	class Meta:
+		model = TodoItem
+		fields = ['task_name', 'priority', 'due_date', 'description']
